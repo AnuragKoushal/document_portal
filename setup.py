81e4e60ae608fcb6244fed5f44f0b60da31abd68
AnuragKoushal <anuragkoushal1993@gmail.com>
2025-09-11 23:47:42 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup (
+    name="Document_Portal",
+    author="Anurag Koushal",
+    author_email="anuragkoushal1993@gmail.com",
+    version= "0.1",
+    packages= find_packages()
+)
